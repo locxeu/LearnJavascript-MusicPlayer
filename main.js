8b59207b7e5ae1d52b7876733ad54b948b5a1e1c
locxeu <loct1k22@gmail.com>
2022-01-21 17:23:22 +0700

    add duration and remaing time

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const song = document.getElementById("song");
 const playBtn = document.querySelector(".player-inner");
 const nextBtn = document.querySelector(".play-forward");
 const prevBtn = document.querySelector(".play-back");
+const durationTime=document.querySelector(".duration");
+const remainingTime=document.querySelector(".remaining");
+
 const listMusic = ["hh.mp3", "ncmlfu.mp3", "sfos.mp3"];
 let isPlaying = true
 let indexSong = 0;
@@ -50,4 +53,24 @@ function playPause() {
         playBtn.innerHTML = '<ion-icon name="play"></ion-icon>';
     }
 
+}
+
+//--------------Set Duration Time--------------//
+function displayTime(){
+const {duration,currentTime}=song;
+remainingTime.textContent=formartTimer(currentTime);
+if(!duration){
+    durationTime.textContent="00:00"
+}else{
+    durationTime.textContent=formartTimer(duration);
+}
+}
+displayTime();
+setInterval(displayTime,1000);
+
+function formartTimer(number){
+    const minute= Math.floor(number/60);
+    const seconds= Math.floor(number-minute*60);
+    
+    return `${minute}:${seconds<10?"0"+seconds:seconds}`;
 }
